18ece1037702c881f5cd35ef0cd26d33a4b830ec
HMontellano <Horacio.montellano@outlook.com>
2024-09-24 16:40:12 -0600

    Add files via upload

---
+++ Account ADT.java
@@ -0,0 +1,39 @@
+public abstract class Account {
+    private String accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    public Account(String accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+}
