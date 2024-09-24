1b182000140dee0c941d6ffe7f7f5518f59f9a4c
HMontellano <Horacio.montellano@outlook.com>
2024-09-24 16:45:34 -0600

    Add files via upload

---
+++ CheckingAccount.java
@@ -0,0 +1,25 @@
+public class CheckingAccount extends Account {
+    private double overdraftLimit;
+
+    public CheckingAccount(String accountNumber, String accountHolder, double balance, double overdraftLimit) {
+        super(accountNumber, accountHolder, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount <= getBalance() + overdraftLimit) {
+            super.withdraw(amount);
+        } else {
+            System.out.println("Overdraft limit exceeded");
+        }
+    }
+
+    public double getOverdraftLimit() {
+        return overdraftLimit;
+    }
+
+    public void setOverdraftLimit(double overdraftLimit) {
+        this.overdraftLimit = overdraftLimit;
+    }
+}
---
+++ SavingsAccount.java
@@ -0,0 +1,21 @@
+public class SavingsAccount extends Account {
+    private double interestRate;
+
+    public SavingsAccount(String accountNumber, String accountHolder, double balance, double interestRate) {
+        super(accountNumber, accountHolder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public void addInterest() {
+        double interest = getBalance() * interestRate;
+        deposit(interest);
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+}
